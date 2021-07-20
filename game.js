784c2c7ca82221c987c90960e1906251470e21a5
NC-CSX <niallcrk@hotmail.co.uk>
2021-07-20 16:30:15 +0000

    Update index.html and game.js

--- game.js
+++ game.js
@@ -22,7 +22,7 @@ function flipCard() {
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    let isMatch = firstCard.dataset.cards === secondCard.dataset.cards;
 
   isMatch ? disableCards() : unflipCards();
 }
